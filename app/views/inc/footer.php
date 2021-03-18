a124cdff573fbedb57043097c5e1d02e266f7d37
timmo <mustonentimmo@gmail.com>
2021-03-18 21:43:08 +0200

    Määratud väljund bootsrapi konteinerisse

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,3 +1,4 @@
+
 </div>
 <script src="<?php echo URLROOT;?>/public/js/main.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
